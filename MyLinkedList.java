cbbaf2f59f564b798a7ba012fad2639c107aa09e
Albert Wan <awan00@stuy.edu>
2018-12-13 11:02:28 -0500

    some code

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,6 +1,15 @@
 import java.util.List;
 public class MyLinkedList{
-  public boolean add(int x){}
-  public void add(int idx, int x){}
-  public int size(){}
+boolean add(Integer value)
+     int size()   
+     String toString()
+*    Integer get(int index) 
+*    Integer set(int index,Integer value)
+
+     boolean contains(Integer value)
+     int indexOf(Integer value)
+
+*    void add(int index,Integer value) 
+*    Integer remove(int index) 
+*    Integer remove(Integer value)
 }
