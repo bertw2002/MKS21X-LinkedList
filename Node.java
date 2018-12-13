07a563f697960772353b6ae24260b990df8ce48c
Albert Wan <awan00@stuy.edu>
2018-12-13 10:47:07 -0500

    node code

--- Node.java
+++ Node.java
@@ -1,1 +1,30 @@
-
+public class Node{
+  private int data;
+  private Node next,prev;
+  public Node(int data, Node next, Node prev){
+    this.data = data;
+    this.next = next;
+    this.prev = prev;
+  }
+  public Node(int data){
+    this.data = data;
+  }
+  public int getData(){
+    return data;
+  }
+  public Node getNext(){
+    return next;
+  }
+  public Node getPrev(){
+    return prev;
+  }
+  public void setData(int data){
+    this.data = data;
+  }
+  public void setNext(Node next){
+    this.next = next;
+  }
+  public void setPrev(Node prev){
+    this.prev = prev;
+  }
+}
