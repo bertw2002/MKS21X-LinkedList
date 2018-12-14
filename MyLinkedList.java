22c918117f6b1bcae496162abc82bd89035cb3c3
Sublime <other@Alberts-MBP.fios-router.home>
2018-12-13 23:16:52 -0500

    added framwork

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,15 +1,33 @@
 import java.util.List;
 public class MyLinkedList{
-boolean add(Integer value)
-     int size()   
-     String toString()
-*    Integer get(int index) 
-*    Integer set(int index,Integer value)
+  public boolean add(Integer value){
 
-     boolean contains(Integer value)
-     int indexOf(Integer value)
+  }
+  public int size(){
 
-*    void add(int index,Integer value) 
-*    Integer remove(int index) 
-*    Integer remove(Integer value)
+  }
+  public String toString(){
+
+  }
+  public Integer get(int index){
+
+  }
+  public Integer set(int index,Integer value){
+
+  }
+  public boolean contains(Integer value){
+
+  }
+  public int indexOf(Integer value){
+
+  }
+  public void add(int index,Integer value){
+    
+  }
+  public Integer remove(int index) {
+
+  }
+  public Integer remove(Integer value){
+
+  }
 }
