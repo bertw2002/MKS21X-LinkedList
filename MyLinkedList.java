d2da59d91bba2007d67601cb54285a5e7bea321c
Sublime <other@Alberts-MBP.fios-router.home>
2018-12-16 18:12:51 -0500

    finished

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -72,20 +72,23 @@ public class MyLinkedList{
   }
   public boolean contains(Integer value){
 		for (int x = 0; x < length; x++){
-			if (getNthNode(x).getData() == value){
+			if (value.equals(getNthNode(x).getData())){
 				return true;
 			}
 		}
 		return false;
   }
   public int indexOf(Integer value){
-		for (Integer x = 0; x < length; x++){
-			if (getNthNode(x).getData() == value){
+		for (int x = 0; x < length; x++){
+			if (value.equals(getNthNode(x).getData())){
 				return x;
 			}
 		}
 		return -1;
   }
+
+
+
 	public void add(int index,Integer value){
 		if (index < 0 && index >= length){
 			throw new IndexOutOfBoundsException();
@@ -106,11 +109,10 @@ public class MyLinkedList{
 			for (int x = 0; x < index - 1; x++) {
         newnode = newnode.next();
       }
-			Node nodenext = newnode.next();
-      Node newNode = new Node(value, node.next(), newnode);
-      newnode.setNext(newNode);
-      newnode = node.next().next();
-      newnode.setPrev(newNode);
+      Node newernode = new Node(value, newnode.next(), newnode);
+      newnode.setNext(newernode);
+      newnode = newnode.next().next();
+      newnode.setPrev(newernode);
       length++;
 
 			//if index == 0;
@@ -126,14 +128,22 @@ public class MyLinkedList{
 		if (index < 0 && index >= length){
 			throw new IndexOutOfBoundsException();
 		}
-		else if (index == size() - 1){
-			Integer oldvar = getNthNode(length - 1).getData();
-			getNthNode(length - 1).setNext(null);
-			end = getNthNode(size() - 1);
+
+		if (index == 0){
+			Integer oldvar = getNthNode(length - 2).getData();
+			getNthNode(0).setPrev(null);
+			start = getNthNode(1);
+			length --;
+			return oldvar;
+		}else if(index == length - 1){
+			Integer oldvar = getNthNode(length - 2).getData();
+			getNthNode(index-1).setNext(null);
+			end = getNthNode(index-1);
 			length--;
 			return oldvar;
+
 		}else{
-			Integer oldvar = getNthNode(length - 1).getData();
+			Integer oldvar = getNthNode(length - 2).getData();
 			getNthNode(index + 1).setPrev(getNthNode(index - 1));
 			getNthNode(index - 1).setNext(getNthNode(index + 1));
 			length --;
@@ -141,6 +151,8 @@ public class MyLinkedList{
 		}
 
   }
+
+
   public boolean remove(Integer value){
 		if (contains(value)){
 			remove(indexOf(value));
